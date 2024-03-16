0a7f7a6646476f0f362ee7f65d097f9b26aced4a
AhmedMurshid <ahmed.abdullahione@gmail.com>
2024-03-16 15:46:34 -0400

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-// import NavigationButtons from './NavSwitch';
+import React from 'react';
 import AppRoutes from './AppRoutes';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppRoutes/>
-      </div>
-    </Router>
+    <div className="App">
+      <AppRoutes/>
+    </div>
   );
 }
 
--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import ParentComponent from './ParentComponent';
 import Dental from './nav/Dental';
 
 const AppRoutes = () => {
   return (
-    <Routes>
-    <Route path="/my-site" element={<ParentComponent />} />
-    <Route path="/my-site/dental" element={<Dental />} />
-  </Routes>
-  
+    <Router>
+      <Routes>
+        <Route path="/" element={<ParentComponent />} />
+        <Route path="/dental" element={<Dental />} />
+        {/* Your other routes */}
+      </Routes>
+    </Router>
   );
 };
 
 export default AppRoutes;
-
